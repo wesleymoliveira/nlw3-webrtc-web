fb93b82d9816a1df789cafa8445c65ad5c25ad43
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-04-18 11:23:36 -0300

    Deploy config on new api project

--- server.js
+++ server.js
@@ -1,64 +1,66 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io');
+const socket = require("socket.io");
 const io = socket(server);
-const username = require('username-generator');
-const path = require('path');
-const cors = require('cors');
-const { AwakeHeroku } = require('awake-heroku');
+const username = require("username-generator");
+const path = require("path");
+const cors = require("cors");
+const { AwakeHeroku } = require("awake-heroku");
 
 AwakeHeroku.add({
-    url: "https://happy-backend-2020.herokuapp.com"
-})
-
+  url: "https://happy-backend-2021.herokuapp.com",
+});
 
 app.use(cors());
 
 //app.use(express());
 
-app.use(express.static('./web/build'));
+app.use(express.static("./web/build"));
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "web","build","index.html"));
-}) 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "web", "build", "index.html"));
+});
 
-const users={}
+const users = {};
 
-io.on('connection', socket => {
-    //gera o id da conexão socket
-    const userid=username.generateUsername('-')
-    if(!users[userid]){
-        users[userid] = socket.id
-    }
-    //emite seu id para outros conectados no socket
-    socket.emit('yourID', userid)
-    io.sockets.emit('allUsers', users)
-    
-    socket.on('disconnect', ()=>{
-        delete users[userid]
-    })
+io.on("connection", (socket) => {
+  //gera o id da conexão socket
+  const userid = username.generateUsername("-");
+  if (!users[userid]) {
+    users[userid] = socket.id;
+  }
+  //emite seu id para outros conectados no socket
+  socket.emit("yourID", userid);
+  io.sockets.emit("allUsers", users);
 
-    socket.on('callUser', (data)=>{
-        io.to(users[data.userToCall]).emit('hey', {signal: data.signalData, from: data.from})
-    })
+  socket.on("disconnect", () => {
+    delete users[userid];
+  });
 
-    socket.on('acceptCall', (data)=>{
-        io.to(users[data.to]).emit('callAccepted', data.signal)
-    })
+  socket.on("callUser", (data) => {
+    io.to(users[data.userToCall]).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
 
-    socket.on('close', (data)=>{
-        io.to(users[data.to]).emit('close')
-    })
+  socket.on("acceptCall", (data) => {
+    io.to(users[data.to]).emit("callAccepted", data.signal);
+  });
 
-    socket.on('rejected', (data)=>{
-        io.to(users[data.to]).emit('rejected')
-    })
-})
+  socket.on("close", (data) => {
+    io.to(users[data.to]).emit("close");
+  });
 
-const port = process.env.PORT || 8000
+  socket.on("rejected", (data) => {
+    io.to(users[data.to]).emit("rejected");
+  });
+});
 
-server.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3333",
-  baseURL: "https://happy-backend-2020.herokuapp.com/",
+  baseURL: "https://happy-backend-2021.herokuapp.com/",
 });
 
 export default api;
